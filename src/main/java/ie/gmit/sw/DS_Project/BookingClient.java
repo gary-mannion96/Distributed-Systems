ddb68cda8ab2619a841847f20d05da925a17a149
Gary Mannion <gkmannion@hotmail.com>
2018-12-10 09:48:44 +0000

    rmi stuff

---
+++ src/main/java/ie/gmit/sw/DS_Project/BookingClient.java
@@ -0,0 +1,37 @@
+package ie.gmit.sw.DS_Project;
+
+import java.rmi.ConnectException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class BookingClient
+{
+
+	public static void main(String[] args) throws NotBoundException, RemoteException
+	{
+
+		try
+		{
+
+			// Create stub
+			BookingRMIServerInterface bookingServiceStub = new BookingRMIServerImpl();
+
+			// Bind the bookingServiceStub to the databaseServuce
+			Naming.rebind("databaseService", bookingServiceStub);
+
+			// Remote methods
+			// bookingServiceStub.getBooking(1);
+			bookingServiceStub.createBooking(null);
+
+		}
+		catch (Exception e)
+		{
+			System.out.println("Could not find registry: " + e);
+		}
+
+	}
+
+}
